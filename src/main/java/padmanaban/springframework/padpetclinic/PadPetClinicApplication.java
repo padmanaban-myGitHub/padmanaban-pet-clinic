b2974337a9ea602dd373cb6e0905d8f3d21a3372
padmanaban <softtest.spring@gmail.com>
2020-12-08 23:05:46 +0530

    Intial Project Add

---
+++ src/main/java/padmanaban/springframework/padpetclinic/PadPetClinicApplication.java
@@ -0,0 +1,13 @@
+package padmanaban.springframework.padpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadPetClinicApplication.class, args);
+    }
+
+}
