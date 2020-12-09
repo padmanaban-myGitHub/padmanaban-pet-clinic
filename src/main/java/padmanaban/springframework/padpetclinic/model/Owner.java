30dc40433395a4d63b88a1fdcf6952486d25ed03
padmanaban <softtest.spring@gmail.com>
2020-12-09 17:41:16 +0530

    adding POJOs. Closes #2

---
+++ src/main/java/padmanaban/springframework/padpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package padmanaban.springframework.padpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/padmanaban/springframework/padpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package padmanaban.springframework.padpetclinic.model;
+
+public class Vet extends Person {
+}
